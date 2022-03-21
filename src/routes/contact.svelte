441f0e395b385b3157ac6c4ea3f02026e5189c71
lemueljay <lemueljayvallinas@gmail.com>
2022-03-22 01:01:17 +0800

    Hide bot field.

--- src/routes/contact.svelte
+++ src/routes/contact.svelte
@@ -34,7 +34,7 @@
         <div class="item">
             <form class="contact-form-grid" name="contactform" method="POST" netlify netlify-honeypot="bot-field">
                 <input type="hidden" name="form-name" value="contactform" />
-                <input type="text" name="bot-field" />
+                <input type="hidden" name="bot-field" />
                 <div class="contact-form-row">
                     <div class="contact-form-label">NAME</div>
                     <input type="text" name="name" class="contact-form-input"/>
