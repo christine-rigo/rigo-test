59e60ae9748e4c07aecebefb7f420efe9fee0bae
lemueljay <lemueljayvallinas@gmail.com>
2022-04-01 02:47:48 +0800

    Add service section.

--- src/routes/nftcon/index.svelte
+++ src/routes/nftcon/index.svelte
@@ -49,8 +49,91 @@ import Logo from '$/components/svg/Logo.svelte';
         </div>
     </section>
 
-    <section class="services-container">
-
+    <section class="services-container" style="position: relative;">
+        <div class="services-div">
+            <div class="service-card" style="background-color: var(--app-color-yellow)">
+                <div class="service-header">+ TECHNICAL</div>
+                <div class="service-offers">
+                    <div class="service-offer">
+                        <div class="service-offer-title">Brochure Websites</div>
+                        <div class="service-offer-desc">Design, UX, SEO, Copy and Development, Everything you need with Rigo.</div>
+                    </div>
+                    <div class="service-offer">
+                        <div class="service-offer-title">Crypto / NFT</div>
+                        <div class="service-offer-desc">Coming soon</div>
+                    </div>
+                    <div class="service-offer"></div>
+                    <div class="service-offer"></div>
+                    <div class="service-offer"></div>
+                </div>
+            </div>
+            <div class="service-card" style="background-color: var(--app-color-green)">
+                <div class="service-header">+ DATA</div>
+                <div class="service-offers">
+                    <div class="service-offer">
+                        <div class="service-offer-title">Marketing Intelligence</div>
+                        <div class="service-offer-desc">Audits and analyse your content and that of your competitors’ to identify gaps and opportunities.</div>
+                    </div>
+                    <div class="service-offer">
+                        <div class="service-offer-title">data dashboard</div>
+                        <div class="service-offer-desc">start by finding your target search terms, them watch your traffic and leads grow as you climb through the ranks.</div>
+                    </div>
+                    <div class="service-offer">
+                        <div class="service-offer-title">seo</div>
+                        <div class="service-offer-desc">start by finding your target search terms, them watch your traffic and leads grow as you climb through the ranks.</div>
+                    </div>
+                    <div class="service-offer"></div>
+                    <div class="service-offer"></div>
+                </div>
+            </div>
+            <div class="service-card" style="background-color: var(--app-color-red)">
+                <div class="service-header">+ CREATIVE</div>
+                <div class="service-offers">
+                    <div class="service-offer">
+                        <div class="service-offer-title">original articles</div>
+                        <div class="service-offer-desc">from idea to publication, thought leadership pieces to showcase your expertise and build trust with your customers</div>
+                    </div>
+                    <div class="service-offer">
+                        <div class="service-offer-title">tone of voice</div>
+                        <div class="service-offer-desc">succinct messaging documents, or full brand consultations to overhaul your tone of voice. Do what’s needed. </div>
+                    </div>
+                    <div class="service-offer">
+                        <div class="service-offer-title">branding</div>
+                        <div class="service-offer-desc">make your new business real with the essential brand assets for launch</div>
+                    </div>
+                    <div class="service-offer">
+                        <div class="service-offer-title">google news</div>
+                        <div class="service-offer-desc">the content and experience needed to get your publication or website accepted to google news</div>
+                    </div>
+                    <div class="service-offer">
+                        <div class="service-offer-title">social</div>
+                        <div class="service-offer-desc">coming soon</div>
+                    </div>
+                </div>
+            </div>
+            <div class="service-card" style="background-color: var(--app-color-lunarblue)">
+                <div class="service-header">+ DIGITAL ADVERTISING</div>
+                <div class="service-offers">
+                    <div class="service-offer">
+                        <div class="service-offer-title">linkedin  campaigns</div>
+                        <div class="service-offer-desc">in-depth audience targeting and standout creative will get your business n front of the right audience</div>
+                    </div>
+                    <div class="service-offer">
+                        <div class="service-offer-title">paid search</div>
+                        <div class="service-offer-desc">highly targeted cost-effective campaigns that drive quality Interactions and attract leads</div>
+                    </div>
+                    <div class="service-offer">
+                        <div class="service-offer-title">digital pr</div>
+                        <div class="service-offer-desc">build relationships and share something that gets people talking</div>
+                    </div>
+                    <div class="service-offer">
+                        <div class="service-offer-title">email marketing</div>
+                        <div class="service-offer-desc">powerful automated email welcome series, nurture your contacts and leads into customers.</div>
+                    </div>
+                    <div class="service-offer"></div>
+                </div>
+            </div>
+        </div>
     </section>
 
     <section class="gif-container">
@@ -166,6 +249,80 @@ import Logo from '$/components/svg/Logo.svelte';
             padding: 0vw 9vw;
             height: 100vh;
         }
+    }
+
+    .services-container {
+        display: flex;
+        flex-direction: column;
+
+        .services-div {
+            margin-left: 2vw;
+            padding-left: 5vw;
+            background: url('/nftcon/services services services services services services services services.svg') repeat-y left top;
+        }
+
+        .service-card {
+            padding: 2vw 4vw;
+            cursor: pointer;
+
+            .service-header {
+                font-family: 'Satoshi';
+                font-style: normal;
+                font-weight: 700;
+                font-size: 50px;
+                line-height: 40px;
+            }
+
+            .service-offers {
+                padding-top: 2vw;
+                display: flex;
+                flex-direction: row;
+
+                .service-offer {
+                    flex: 1 0 20%;
+                    .service-offer-title {
+                        font-family: 'Bebas Neue';
+                        font-style: normal;
+                        font-weight: 400;
+                        font-size: 25px;
+                        line-height: 25px;
+                        color: #12182E;
+                        text-transform: uppercase;
+                    }
+                    .service-offer-desc {
+                        padding-top: 1vw;
+                        font-family: 'Satoshi';
+                        font-style: normal;
+                        font-weight: 400;
+                        font-size: 13px;
+                        line-height: 140%;
+                        color: #12182E;
+                        text-transform: uppercase;
+                    }
+                }
+            }
+        }
+    }
+    @media(max-width: 425px) {
+        .services-container {
+
+            .services-div {
+                margin-left: 0vw;
+                padding-left: 0vw;
+            }
+            .service-card {
+                padding: 4vw 4vw;
+                .service-offers {
+                    padding: 12vw 0vw;
+                    flex-direction: column;
+                    gap: 4vw;
+                    .service-offer {
+                        flex: 1 0 100%;
+                    }
+                }
+            }
+        }
+       
     }
 
     .gif-container {
