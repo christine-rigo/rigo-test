f80c256c99b5f214ef9637d0c0705b38633d4079
Nick Farr <nick@farrcrysis.co.uk>
2022-11-04 15:54:07 +0000

    Remove Halloween theme

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -29,19 +29,14 @@ import HomeGrid from '$/components/home/HomeGrid.svelte';
 
 <svelte:body use:style={{ background: 'var(--app-color-navy)' }} />
 
-<div class="homepage " style="position: relative;">
+<div class="homepage stop-scrolling" style="position: relative;">
     <div class="rigoprelaunch animate">
-        <div class="prelaunch-logo" style="width:97%; max-width:450px;margin-bottom:-1.5vw;background:url(/static/home/bats.png) center top / contain no-repeat;text-align:center; margin-bottom:-4vw;"><a style="display:flex; justify-content:center; align-items:center; width: 35vw; max-width:150px; margin:1rem auto 2rem;" href="/"><Logo scale={2} /></a></div>
+        <div class="prelaunch-logo"><a href="/"><Logo scale={2} /></a></div>
         <div class="prelaunch-title">
             <div class="prelaunch-heading">Your climb is ours.<br>We're here to help your business reach its potential.</div>
-            <div class="prelaunch-subheading"><strong style="color: #FFF">/ˈrɛdʒəʊ/ rigo  –  to ascend</strong>. rigo means to <i style="">ascend</i> in Igbo; co-founder Stephen’s family language.</div>
+            <div class="prelaunch-subheading"><strong style="color: var(--app-color-lunarblue)">/ˈrɛdʒəʊ/ rigo  –  to ascend</strong>. rigo means to <i style="color: var(--app-color-lunarblue)">ascend</i> in Igbo; co-founder Stephen’s family language.</div>
         </div>
-        <div style="position:relative;">
-            <div class="prelaunch-btn" id="rigo-mode" style="background:white;border-radius:50px;">
-                <ButtonRigoPopup btnColor="btn-white" cta="Discover More"></ButtonRigoPopup>
-            </div>
-            <img style="position:absolute; right:0; transform:translateX(130%) translateY(-70%); width:clamp(4rem,18vw,100px);" src="/static/home/pumpkin.png" alt="" srcset="">
-        </div>
+        <div class="prelaunch-btn" id="rigo-mode"><ButtonRigoPopup btnColor="btn-lunarblue" cta="Discover More"></ButtonRigoPopup></div>
     </div>
 
     <div class="home-container hidden">
@@ -77,10 +72,9 @@ import HomeGrid from '$/components/home/HomeGrid.svelte';
         transition: all 1.25s ease-in;
 
         position: absolute;
-        min-height: 100vh;
+        height: 100vh;
         width: 100vw;
-        // background-color: var(--app-color-lunarblue-contrast);
-        background-color: #504DE6;
+        background-color: var(--app-color-lunarblue-contrast);
         z-index: 99999;
 
         display: flex;
@@ -89,26 +83,14 @@ import HomeGrid from '$/components/home/HomeGrid.svelte';
         align-items: center;
         gap: 4vw;
 
-        padding-bottom: 4rem;
-
         div {
-            // color: var(--app-color-lunarblue);
-            color: #FFF;
+            color: var(--app-color-lunarblue);
         }
     }
 
     .prelaunch-logo {
-        padding-top: clamp(7rem, 5.5vw, 160px);
-        padding-bottom: clamp(2rem, 3vw, 6rem);
-
-        @media screen and (max-width: 768px) {
-            padding-top: clamp(5rem, 5.5vw, 160px);
-            padding-bottom: clamp(2rem, 3vw, 6rem);
-        }
-
         a {
-            // --color: var(--app-color-lunarblue);
-            --color: #FFF;
+            --color: var(--app-color-lunarblue);
         }
     }
 
