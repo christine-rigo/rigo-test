f6cd067df440c7ea1683d7f016ca9999d42c4248
ploiid10 <flloyd.dev@gmail.com>
2022-12-07 21:04:04 +0800

    add thank-you page

---
+++ src/routes/thank-you/__layout.svelte
@@ -0,0 +1,59 @@
+<script lang="ts">
+	import Nav from '$/components/Nav.svelte';
+	import '$/css/global.scss';
+</script>
+
+<main>
+	<aside>
+		<Nav logoScale={1}/>
+	</aside>
+
+	<section>
+		<section class="container">
+			<slot />
+		</section>
+	</section>
+</main>
+
+<style lang="scss">
+	main {
+		min-height: 100%;
+		width: 100%;
+	}
+
+	aside {
+		position: static;
+		top: 0;
+		left: 0;
+		z-index: 9999;
+
+		display: flex;
+		justify-content: center;
+
+		padding: 6% 5%;
+
+		height: unset;
+		width: unset;
+	}
+
+	main > section {
+		min-height: 100%;
+		width: 100%;
+	}
+
+	.container {
+		width: 100%;
+		height: 100%;
+		min-height: 100%;
+		// padding: 0rem 2rem 2rem 2rem;
+	}
+
+	@media(min-width: 1024px) {
+		aside {
+			position: absolute;
+		}
+		.container {
+			padding: 5% 15vw;
+		}
+	}
+</style>
---
+++ src/routes/thank-you/index.svelte
@@ -0,0 +1,52 @@
+<script lang="ts">
+  import Button from '$/components/thank-you/Button.svelte';
+  import Footer from '$/components/Footer.svelte';
+import { navColor } from '$/stores/nav.store';
+import { onMount } from 'svelte';
+import { style } from 'svelte-body';
+
+onMount(() => {
+  $navColor = 'var(--app-color-lunarblue)';
+});
+</script>
+
+<svelte:body use:style={{ background: 'var(--app-color-lunarblue-contrast)' }} />
+
+<svelte:head>
+<title> Thank you / Rigo Agency</title>
+</svelte:head>
+
+<div>
+  <div class="main-content">
+    <h1>Thank you.</h1>
+    <h2>WE WILL BE IN CONTACT SHORTLY.</h2>
+    <Button />
+  </div>
+  <Footer href="/casestudy/gallagher">Gallagher</Footer>
+</div>
+
+<style lang="scss">
+  * {
+    --color: var(--app-color-lunarblue);
+    --color-contrast: var(--app-color-lunarblue-contrast);
+
+    color: var(--color);
+  }
+
+
+  .main-content {
+    text-align: center;
+    padding-bottom: 4rem;
+    margin-bottom: 6rem;
+  }
+
+  h1 {
+    font-size: 100px;
+  }
+
+  h2 {
+    margin: 4rem 0;
+    font-size: 24px;
+  }
+
+</style>
