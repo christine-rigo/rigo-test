eceb7876f5322554a45a49e5941667cd509796ff
Christa <gallegochrista@gmail.com>
2023-05-15 17:20:46 +0800

    setup new technology page

---
+++ src/components/technology/FullpageSection.svelte
@@ -0,0 +1,63 @@
+<script lang="ts">
+	export let step = '';
+	export let title = '';
+</script>
+
+<section class="fp-section">
+	<div class="top-heading">
+		<slot name="heading" />
+	</div>
+
+	<div class="title">
+		<div class="step">Step {step}</div>
+		<div class="text">{title}</div>
+	</div>
+
+	<div class="content">
+		<slot name="content" />
+	</div>
+
+	<div class="icon">
+		<slot name="icon" />
+	</div>
+</section>
+
+<style lang="scss">
+	.fp-section {
+		position: relative;
+		min-height: 100vh;
+		background-color: var(--fp-bg-color);
+		text-align: center;
+		padding-top: 10vh;
+		padding-bottom: 10vh;
+		overflow: hidden;
+	}
+	.top-heading {
+		font-family: 'Bebas Neue', sans-serif;
+		color: var(--fp-top-heading);
+		font-size: 50px;
+	}
+	.title {
+		font-family: 'Bebas Neue', sans-serif;
+		color: #12182e;
+		font-size: 320px;
+		line-height: 0.8;
+		text-transform: uppercase;
+		.step {
+			-webkit-text-fill-color: transparent;
+			-webkit-text-stroke: 1px;
+		}
+	}
+	.content {
+		color: #12182e;
+		max-width: 727px;
+		margin: 0 auto;
+	}
+	.icon {
+		position: absolute;
+		top: 0;
+		left: 0;
+		width: 100%;
+		height: 100%;
+	}
+</style>
---
+++ src/routes/technology-2/__layout.svelte
@@ -0,0 +1,47 @@
+<script lang="ts">
+	import Nav from '$/components/Nav.svelte';
+	import '$/css/global.scss';
+</script>
+
+<main>
+	<aside>
+		<Nav logoScale={1} navItemsStyle="align-items: flex-start;" />
+	</aside>
+
+	<section class="container">
+		<slot />
+	</section>
+</main>
+
+<style lang="scss">
+	main {
+		min-height: 100%;
+		width: 100%;
+	}
+
+	aside {
+		position: relative;
+		top: 0;
+		left: 0;
+		z-index: 9999;
+
+		display: flex;
+		justify-content: center;
+
+		padding: 6% 5% 6% 5%;
+	}
+
+	section {
+		min-height: 100%;
+		width: 100%;
+	}
+
+	@media (min-width: 1024px) {
+		aside {
+			position: absolute;
+			width: 20vw;
+			height: 100%;
+			padding: 3rem 0;
+		}
+	}
+</style>
