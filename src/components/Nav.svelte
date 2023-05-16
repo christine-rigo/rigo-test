5829f101a475f5c595ea95906d877ee35929cd00
Nick Farr <nick@farrcrysis.co.uk>
2023-05-16 17:46:33 +0100

    Contact button

--- src/components/Nav.svelte
+++ src/components/Nav.svelte
@@ -4,7 +4,7 @@
 
 	export let logoScale = 1.8;
 	export let navigationBarColor = '';
-	export let navItemsStyle = 'align-items: center;'
+	export let navItemsStyle = 'align-items: center;';
 </script>
 
 <div class="nav-container" style="--color: {navigationBarColor || $navColor}">
@@ -18,11 +18,44 @@
 		<a href="/services" sveltekit:prefetch>Services</a>
 		<a class="hidden-xs" href="/about" sveltekit:prefetch>About</a>
 		<!-- <a href="/team" sveltekit:prefetch>Team</a> -->
-		<a href="/contact">Contact</a>
+		<!-- <a href="/contact">Contact</a> -->
+		<button
+			class="contact-button-tf"
+			data-tf-popup="EHpJH0kW"
+			data-tf-opacity="100"
+			data-tf-size="100"
+			data-tf-iframe-props="title=Rigo Landing Page Trial"
+			data-tf-transitive-search-params
+			data-tf-medium="snippet"
+			style="all:unset;font-family:Helvetica,Arial,sans-serif;display:inline-block;max-width:100%;white-space:nowrap;overflow:hidden;text-overflow:ellipsis;background-color:#0445AF;color:#fff;font-size:20px;border-radius:25px;padding:0 33px;font-weight:bold;height:50px;cursor:pointer;line-height:50px;text-align:center;margin:0;text-decoration:none;"
+			>Contact</button
+		><script src="//embed.typeform.com/next/embed.js"></script>
 	</nav>
 </div>
 
 <style lang="scss">
+	.contact-button-tf {
+		all: unset !important;
+		font-family: Helvetica, Arial, sans-serif !important;
+		display: inline-block !important;
+		max-width: 100% !important;
+		white-space: nowrap !important;
+		overflow: hidden !important;
+		text-overflow: ellipsis !important;
+		background-color: #ffffff !important;
+		color: #504de6 !important;
+		font-size: 15px !important;
+		border-radius: 25px !important;
+		padding: 6px 23px !important;
+		font-weight: bold !important;
+		height: auto !important;
+		cursor: pointer !important;
+		/* line-height: 50px; */
+		text-align: center !important;
+		margin: 0 !important;
+		text-decoration: none !important;
+	}
+
 	.nav-container {
 		display: flex;
 		flex-direction: row;
@@ -34,7 +67,7 @@
 			transition: fill 0.2s ease-in;
 		}
 	}
-	@media(max-width: 1023px) {
+	@media (max-width: 1023px) {
 		.nav-container {
 			padding-left: 2rem;
 			padding-right: 2rem;
@@ -58,7 +91,7 @@
 		width: max-content;
 	}
 
-	@media(min-width: 1024px) {
+	@media (min-width: 1024px) {
 		nav {
 			flex-direction: column;
 		}
@@ -66,7 +99,7 @@
 			flex-direction: column;
 		}
 	}
-	@media(min-width: 768px) {
+	@media (min-width: 768px) {
 		.nav-container {
 			gap: 2rem;
 		}
@@ -79,12 +112,12 @@
 			font-size: 16px;
 		}
 	}
-	@media(max-width: 1024px) {
+	@media (max-width: 1024px) {
 		.hidden-xs {
 			display: none;
 		}
 	}
-	@media(max-width: 768px) {
+	@media (max-width: 768px) {
 		.nav-container > a {
 			transform: scale(0.7);
 			transform-origin: left;
