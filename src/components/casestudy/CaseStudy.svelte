b0d023a7e57906fff84c73580fc19fa3cd788ca3
lemueljay <lemueljayvallinas@gmail.com>
2023-05-20 14:42:14 +0800

    Add new case studies layout.

--- src/components/casestudy/CaseStudy.svelte
+++ src/components/casestudy/CaseStudy.svelte
@@ -1,70 +1,29 @@
 <script lang="ts">
 	import Nav from "../Nav.svelte";
-	export let navigationBarColor = '';
+	import Button from '$/components/casestudy/Button.svelte';
+
 	export let title = '';
 	export let description = '';
-	export let coverPhoto = '';
-	export let columnSeparator = '';
-	export let hdrSvgPath1:string;
-	export let hdrSvgPath2:string;
-	export let hdrSvgPath3:string;
-	export let hdrSvgPath4:string;
-	export let hdrInfo1:string;
-	export let hdrInfo2:string;
-	export let hdrInfo3:string;
-	export let hdrInfo4:string;
+	export let backgroundColor:string;
+	export let color:string;
+	export let btnColor:string;
+	export let borderColor:string = 'var(--app-color-lunarblue)';
+	export let subheadingColor:string = 'var(--app-color-navy)';
 </script>
 
-<div class="container" style="background: linear-gradient(10deg, #12182D 30%, rgba(18, 24, 45, 0) 100%), url({coverPhoto}) no-repeat top center/cover;">
+<div class="container" style="background-color: {backgroundColor}; color: {color}">
 	<aside>
-		<Nav logoScale={1} navigationBarColor={navigationBarColor} navItemsStyle="align-items: flex-start;" />
+		<Nav logoScale={1} navigationBarColor={'white'} navItemsStyle="align-items: flex-start;" />
 	</aside>
 	<div class="main">
-		<div class="categories">
-			<div>+ Creative</div>
-			<div>+ Digital Advertising</div>
-		</div>
 		<div class="heading">
 			{title}     
 		</div>
-		<div class="subheading">
-			{description}
+		<div class="subheading" style="color:{subheadingColor}">
+			{@html description}
 		</div>
-		<div class="hidden-desktop">
-			<svg width="310" height="1" viewBox="0 0 310 1" fill="none" xmlns="http://www.w3.org/2000/svg">
-				<line x1="0.461914" y1="0.5" x2="309.386" y2="0.5" stroke="#EE6338"/>
-			</svg>				
-		</div>
-		<div class="adtagscontainer">
-			<img class="hidden-mobile" src={columnSeparator} alt="rigo">
-			<div class="adtags">
-				<div class="adtagtitle">
-					<img src={hdrSvgPath1} alt="rigo">
-				</div>
-				<div class="adtaginfo" style="color: {navigationBarColor}">{hdrInfo1}</div>
-			</div>
-			<img class="hidden-mobile" src={columnSeparator} alt="rigo">
-			<div class="adtags">
-				<div class="adtagtitle">
-					<img src={hdrSvgPath2} alt="rigo">
-				</div>
-				<div class="adtaginfo" style="color: {navigationBarColor}">{hdrInfo2}</div>
-			</div>
-			<img class="hidden-mobile" src={columnSeparator} alt="rigo">
-			<div class="adtags">
-				<div class="adtagtitle">
-					<img src={hdrSvgPath3} alt="rigo">
-				</div>
-				<div class="adtaginfo" style="color: {navigationBarColor}">{hdrInfo3}</div>
-			</div>
-			<img class="hidden-mobile" src={columnSeparator} alt="rigo">
-			<div class="adtags">
-				<div class="adtagtitle">
-					<img src={hdrSvgPath4} alt="rigo">
-				</div>
-				<div class="adtaginfo" style="color: {navigationBarColor}">{hdrInfo4}</div>
-			</div>
-			<img class="hidden-mobile" src={columnSeparator} alt="rigo">
+		<div>
+			<Button btnColor="{btnColor}" borderColor="{borderColor}" />
 		</div>
 	</div>
 </div>
@@ -73,9 +32,9 @@
 	.container {
 		background-size: cover;
 		background-color: var(--app-color-navy);
-		color: var(--app-color-red);
-		height: 472px;
+		color: var(--app-color-lunarblue);
 		position: relative;
+		width: 100vw;
 	}
 
 	aside {
@@ -90,21 +49,24 @@
 		padding: 6% 5% 6% 5%;
 	}
 
+	.main {
+		padding: 6% 15vw 35vh 15vw;
+		display: flex;
+		flex-direction: column;
+		gap: 2.5vw;
+	}
+
 	@media(max-width: 768px) {
 		aside {
 			position: relative;
 		}
 
 		.container {height: auto;}
+		.main {
+			padding: 6% 15vw 15vh 15vw;
+		}
 	}
 
-	.main {
-		padding: 6% 15vw 6% 15vw;
-
-		display: flex;
-		flex-direction: column;
-		gap: 2.5vw;
-	}
 	.categories {
 		display: flex;
 		flex-direction: column;
@@ -118,20 +80,19 @@
 		color: var(--app-color-lunarblue-contrast);
 	}
     .heading {
-        font-family: "Satoshi";
+        font-family: "Bebas Neue", "Satoshi";
 		font-style: normal;
-		font-weight: bold;
-		font-size: 100px;
-		line-height: 100px;
+		font-weight: 700;
+		font-size: 130px;
+		line-height: 80%;
     }
 	.subheading {
-		font-family: 'Bebas Neue';
+		font-family: 'Satoshi';
 		font-style: normal;
 		font-weight: normal;
-		font-size: 24px;
-        line-height: 110%;
-        max-width: 690px;
-		color: var(--app-color-lunarblue-contrast);
+		font-size: 18px;
+        line-height: 140%;
+		color: var(--app-color-navy);
 	}
 
 	.adtagscontainer {
@@ -153,7 +114,7 @@
 				font-weight: normal;
 				font-size: 12px;
 				line-height: 140%;
-				color: var(--app-color-red);
+				color: var(--app-color-lunarblue);
 			}
 		}
 	}
@@ -162,6 +123,10 @@
     }
 
     @media(max-width: 425px) {
+		.main {
+			align-items: center;
+			padding: 6% 15vw 15vh 15vw;
+		}
 		.hidden-mobile {
 			display: none;
 		}
@@ -175,8 +140,8 @@
             gap: 1vw;
         }
 		.heading {
-			font-size: 50px;
-			line-height: 100px;
+			font-size: 80px;
+			line-height: 80%;
 		}
         .subheading {
             font-size: 20px;
---
+++ src/components/casestudy/StatsCard.svelte
@@ -0,0 +1,70 @@
+<script>
+    export let statistics = [];
+    export let backgroundColor = "white";
+    export let color = "#181818";
+</script>
+
+<div class="stats-section">
+    <div class="stats-container">
+        {#each statistics as statistic}
+        <div class="stats-card" style="background: {backgroundColor}; color: {color}">
+            <div class="stats-lead">{statistic.stats}</div>
+            <div class="stats-title">{@html statistic.statsTitle}</div>
+        </div>
+        {/each}
+    </div>
+</div>
+
+<style lang="scss">
+    .stats-section {
+        display: flex;
+        justify-content: flex-end;
+        padding: 0rem 4rem 0rem 4rem;
+    }
+    .stats-container {
+        display: grid;
+        grid-template-columns: 1fr 1fr;
+        grid-template-rows: 1fr 1fr;
+        gap: 10px; /* Optional: Set the gap between grid items */
+        position: relative;
+        bottom: -6vh;
+        right: 5%;
+    }
+    .stats-card {
+        background: #3BF0C8;
+        border-radius: 24.4297px;
+        padding: 3rem 3rem;
+    }
+    .stats-lead {
+        font-family: 'Bebas Neue';
+        font-style: normal;
+        font-weight: 700;
+        font-size: 32px;
+        line-height: 30px;
+        text-align: center;
+    }
+    .stats-title {
+        font-family: 'Satoshi';
+        font-style: normal;
+        font-weight: 500;
+        font-size: 13px;
+        line-height: 140%;
+        text-align: center;
+    }
+    @media(max-width: 768px) {
+        .stats-section {
+            padding: 1rem 2rem 1rem 2rem;
+            justify-content: center;
+        }
+        .stats-container {
+            bottom: 0px;
+            right: 0%;
+        }
+	}
+
+	@media(max-width: 425px) {
+        .stats-card {
+            padding: 2rem;
+        }
+	}
+</style>
