1342281551adef712194f91697d460bcf2c0acd9
lemueljay <lemueljayvallinas@gmail.com>
2023-08-01 23:42:09 +0800

    Open new contact form on start your ascent btn.

--- src/components/casestudy/Button.svelte
+++ src/components/casestudy/Button.svelte
@@ -3,9 +3,18 @@
 	export let btnColor:string;
 </script>
 
-<a href="/contact" style="color: {borderColor}">
-	<button style="border: 1px solid {borderColor};" class="startyourascentbutton {btnColor}">Start Your Ascent</button>
-</a>
+<!-- This can be commented out as long as Rigo Nav is present -->
+<!-- <script src="//embed.typeform.com/next/embed.js"></script> -->
+
+<button
+	style="color: {borderColor}; border: 1px solid {borderColor}"
+	class="contact-button-tf startyourascentbutton {btnColor}"
+	data-tf-popup="EHpJH0kW"
+	data-tf-opacity="100"
+	data-tf-size="100"
+	data-tf-iframe-props="title=Rigo Landing Page Trial"
+	data-tf-transitive-search-params
+	data-tf-medium="snippet">Start your ascent</button>
 
 <style lang="scss">
     button {
@@ -25,24 +34,24 @@
 	.btn-red {
 		transition: background-color 150ms ease-in;
 		&:hover {
-			background-color: var(--app-color-red);
-            color: white;
+			background-color: var(--app-color-red) !important;
+            color: white !important;
 		}
 	}
 
 	.btn-lunarblue {
 		transition: background-color 150ms ease-in;
 		&:hover {
-			background-color: var(--app-color-lunarblue);
-            color: white;
+			background-color: var(--app-color-lunarblue) !important;
+            color: white !important;
 		}
 	}
 
 	.btn-white {
 		transition: background-color 150ms ease-in;
 		&:hover {
-			background-color: white;
-            color: black;
+			background-color: white !important;
+            color: black !important;
 		}
 	}
 </style>
